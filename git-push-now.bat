e74dd0f97eac1bfac4d5367591712b9c6f93322b
favoranu <favoranu@users.noreply.github.com>
2026-06-08 02:13:30 -0600

    Update push script

--- git-push-now.bat
+++ git-push-now.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
-"C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" status
-"C:\Program Files\Git\cmd\git.exe" commit -m "Add Turnstile, fix Pages project name, remove invalid redirects"
+"C:\Program Files\Git\cmd\git.exe" add git-push-now.bat
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update push script" 2>nul
+"C:\Program Files\Git\cmd\git.exe" stash push -u -m "temp" 2>nul
+"C:\Program Files\Git\cmd\git.exe" pull origin main --rebase
 "C:\Program Files\Git\cmd\git.exe" push origin main
+"C:\Program Files\Git\cmd\git.exe" stash pop 2>nul
